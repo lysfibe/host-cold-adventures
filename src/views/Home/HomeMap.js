1622b646e2141a1bf776a504afbe261dece69746
Louis Capitanchik <contact@louiscap.co>
2017-04-01 13:33:49 +0100

    Default to user location

--- src/views/Home/HomeMap.js
+++ src/views/Home/HomeMap.js
@@ -1,8 +1,9 @@
 // @flow
 import React, { PureComponent } from 'react'
-import { View, Text, Dimensions } from 'react-native'
+import { View, Text, Dimensions, StyleSheet } from 'react-native'
 import MapView from 'react-native-maps'
 import mapstyle from './mapstyle'
+import { colors } from '../../theme'
 
 export type Region = {
 	longitude: number,
@@ -31,17 +32,54 @@ export default class HomeMap extends PureComponent {
 		}
 	}
 
+	componentWillMount() {
+		navigator.geolocation.getCurrentPosition(
+			({coords}) => this.setLatLong(coords),
+			(...args) => console.log(args) || alert('Oops!'),
+			{enableHighAccuracy: true}
+		)
+	}
+
 	render() {
-		const { width, height } = Dimensions.get('window')
+		const {width, height} = Dimensions.get('window')
 		return (
-			<View>
+			<View style={{flex: 1}}>
 				<MapView
 					region={this.state.region}
-					onRegionChange={region => console.log(region) || this.setState({ region })}
+					onRegionChange={region => this.setState({region})}
 					customMapStyle={mapstyle}
-					style={{ width, height: height - 25 }}
+					style={{width, height: height - 24}}
 				/>
+				<View style={[styles.verticalLine, {left: width / 2}]}/>
+				<View style={[styles.horizontalLine, {top: height / 2}]}/>
 			</View>
 		)
 	}
+
+	setLatLong = ({latitude, longitude}) =>
+		this.setState(state => ({
+			region: {
+				...state.region,
+				latitude,
+				longitude,
+			}
+		}))
 }
+
+
+const styles = StyleSheet.create({
+	verticalLine: {
+		borderRightWidth: StyleSheet.hairlineWidth,
+		borderRightColor: colors.shades.offwhite,
+		position: 'absolute',
+		top: 0,
+		bottom: 0,
+	},
+	horizontalLine: {
+		borderTopWidth: StyleSheet.hairlineWidth,
+		borderTopColor: colors.shades.offwhite,
+		position: 'absolute',
+		left: 0,
+		right: 0,
+	},
+})
